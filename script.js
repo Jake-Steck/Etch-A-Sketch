799f1296fd59613bdf480a27c43a93eb971b72e6
Jake Steck <60509027+jrsteck@users.noreply.github.com>
2020-12-21 12:35:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const clear = document.getElementsByClassName('button')[0];
 
 
 function grid() {
-    for(let i = 0; i < 4; i++) {
-        for(let j = 0; j < 4; j++) {
+    for(let i = 0; i < 16; i++) {
+        for(let j = 0; j < 16; j++) {
              square = document.createElement('div');
             square.className = 'square';
             display.appendChild(square);
