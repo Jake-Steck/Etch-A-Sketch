bbb6df64d35b1c39afdb9649237ea4554fb80d9a
Jake Steck <60509027+jrsteck@users.noreply.github.com>
2021-01-16 11:23:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const clear = document.getElementsByClassName('button')[0];
 function grid() {
     for(let i = 0; i < 16; i++) {
         for(let j = 0; j < 16; j++) {
-             square = document.createElement('div');
+            square = document.createElement('div');
             square.className = 'square';
             display.appendChild(square);
         }
@@ -16,3 +16,17 @@ function grid() {
 };
 
 grid();
+
+//box.addEventListener("mouseover", hoverBox)
+boxes = document.querySelectorAll(".square")
+
+
+$(".square").on("mouseover", function(){
+    $(this).css("background-color", "black");
+});
+
+function clearButton() {
+    $(".square").css("background-color", "rgb(150, 150, 150)");
+}
+ 
+
